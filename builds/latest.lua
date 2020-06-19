650ca4f3b2ca7f2e795021b95959b524391ed96f
GitHub Action <action@github.com>
2020-06-19 08:55:04 +0000

    ci: Automated build

--- builds/latest.lua
+++ builds/latest.lua
@@ -21,11 +21,15 @@ module.subRoomAdmins = {}
 if module.isTribeHouse then
     for name, player in next, tfm.get.room.playerList do
         if player.tribeName == module.room then
-            module.subRoomAdmins[#module.subRoomAdmins + 1] = name
+            module.subRoomAdmins[name] = true
         end
     end
 else
-    module.subRoomAdmins = {module.roomAdmin}
+    if module.roomAdmin ~= "" then module.subRoomAdmins = {[module.roomAdmin] = true} end
+end
+
+for admin in next, module.admins do
+    module.subRoomAdmins[admin] = true
 end
 
 -- [[ utilites]] --
@@ -66,7 +70,14 @@ end
 local translations = {}
 
 translations.en = {
-    welcome = "\n<N><p align='center'>Welcome to <b><D>#castle</D></b> - Kings' living lounge and lab\nReport any bugs or new features to <b><O>King_seniru</O><G><font size='8'>#5890</font></G></b>\n\nType <b><BV>!modes</BV></b> to check out submodes of this module</p></N>\n"
+    welcome = "<br><N><p align='center'>Welcoem to <b><D>#castle</D></b> - King`s living lounge and lab<br>Report any bugs or new features to <b><O>King_seniru</O><G><font size='8'>#5890</font></G></b><br><br>Type <b><BV>!modes</BV></b> to check out submodes of this module</p></N><br>",
+    modestitle = "<p align='center'><D><font size='16' face='Lucida console'>Castle - submodes</font></D><br><br>",
+    modebrief = "<b>castle0${name}</b> <CH><a href='event:modeinfo:${name}'>ⓘ</a></CH>\t<a href='event:play:${name}'><T>( Play )</T></a><br>",
+    modeinfo = "<p align='center'><D><font size='16' face='Lucida console'>#castle0${name}</font></D></p><br><i><font color='#ffffff' size='14'>“<br>${description}<br><p align='right'>”</p></font></i><b>Author: </b> ${author}<br><b>Version: </b> ${version}</font><br><br><p align='center'><b><a href='event:play:${name}'><T>( Play )</T></a></b></p><br><br><a href='event:modes'>« Back</a>",
+    new_roomadmin = "<N>[</N><D>•</D><N>] </N><D>${name}</D> <N>is now a room admin!</N>",
+    error_auth = "<N>[</N><R>•</R><N>] Error: Authorisation",
+    welcome0graphs = "<br><N><p align='center'>Welcoem to <b><D>#castle0graphs</D></b><br>Report any bugs or suggest interesting functions to <b><O>King_seniru</O><G><font size='8'>#5890</font></G></b><br><br>Type <b><BV>!commands</BV></b> to check out the available commands</p></N><br>",
+    cmds0graphs = "<BV>!admin <name></BV> - Makes a player admin <R><i>(admin only command)</i></R>",
 }
 
 
@@ -87,6 +98,7 @@ modes.castle = {
 
 modes.castle.main = function()
 
+    system.disableChatCommandDisplay()
     tfm.exec.disableAfkDeath()
     tfm.exec.disableAutoNewGame()
     tfm.exec.disableAutoShaman()
@@ -118,7 +130,6 @@ modes.castle.main = function()
         if not closeSequence[id] then return end
         local sequence = closeSequence[id][target or "*"]
         for _, imgid in next, sequence.images do
-            print(imgid)
             tfm.exec.removeImage(imgid)
         end
         for _, txtareaid in next, sequence.txtareas do
@@ -128,13 +139,24 @@ modes.castle.main = function()
 
     -- [[main functions]] --
     displayModes = function(target)
-        local res = "<p align='center'><D><font size='16' face='Lucida console'>Castle - submodes</font></D><br><br>"
+        local commu = tfm.get.room.playerList[target].community
+        local res = module.translate("modestitle", commu)
         for name, mode in next, modes do
             if name ~= "castle" then
-                res = res .. "<b>castle0" .. name .. "</b> <a href='event:modeinfo:" .. name .. "'>ⓘ</a>\t<a href='event:play:" .. name .. "'>( Play )</a><br>"
+                res = res .. module.translate("modebrief", commu, nil, {name = name})
             end
         end
+        handleCloseButton(1, target)
         addTextArea(1, res .. "</p>", target, 200, 100, 400, 200, true)
+    end
+
+    displayMode = function(target, mode)
+        local commu = tfm.get.room.playerList[target].community
+        local name = mode
+        local mode = modes[mode]
+        local res = module.translate("modeinfo", commu, nil, {name = name, author = mode.author, version = mode.version, description = mode.description})
+        handleCloseButton(1, target)
+        addTextArea(1, res, target, 200, 100, 400, 200, true)
     end
 
     -- [[events]] --
@@ -146,6 +168,7 @@ modes.castle.main = function()
     eventNewPlayer = function(name)
         local commu = tfm.get.room.playerList[name].community
         tfm.exec.chatMessage(module.translate("welcome", commu), name)
+        tfm.exec.respawnPlayer(name)
     end 
 
     eventChatCommand = function(name, cmd)
@@ -157,25 +180,187 @@ modes.castle.main = function()
 
     eventTextAreaCallback = function(id, name, evt)
         if evt == "close" then
-            handleCloseButton(id, name)
+            handleCloseButton(id / 1000, name)
+        elseif evt == "modes" then
+            displayModes(name)
         elseif evt:find("%w+:%w+") then
             local key, value = table.unpack(string.split(evt, ":"))
             if key == "play" then
-                tfm.exec.chatMessage("<N>[</N><CS>•</CS><N>]</N> /room #castle0" .. value .. "@" .. name .. "</CS>", name)
+                tfm.exec.chatMessage("<N>[</N><D>•</D><N>]</N><D> /room #castle0" .. value .. "@" .. name .. "</D>", name)
+            elseif key == "modeinfo" then
+                displayMode(name, value)
             end
         end
     end
+
+    eventPlayerDied = tfm.exec.respawnPlayer
 
 end
 
 modes.graphs = {
 	version = "v0.0.1",
-	description = "",
+	description = "Play with graphs! Feel the beauty of math",
 	author = "King_seniru#5890"
 }
 
-modes.graphs.main = function() end
+modes.graphs.main = function()
+    --[[ dependencies]]--
+    --[ linegraph-tfm ]--
+    local a,b,c,d,e,f;local g='0123456789abcdef'function num2hex(h)local i=''while h>0 do local j=math.fmod(h,16)i=string.sub(g,j+1,j+1)..i;h=math.floor(h/16)end;return string.upper(i==''and'0'or i)end;function split(i,k)result={}for l in(i..k):gmatch("(.-)"..k)do table.insert(result,l)end;return result end;function c(m)local n=m[1]for o=1,#m do o=m[o]if o<n then n=o end end;return n end;function d(m)local p=m[1]for o=1,#m do o=m[o]if o>p then p=o end end;return p end;function e(m,q)local r={}for s,o in next,m do r[s]=q(o)end;return r end;function f(t,u,v)local w=table.insert;local r={}for x=t,u,v do w(r,x)end;return r end;local function y(z)return 400-z end;a={}a.__index=a;setmetatable(a,{__call=function(A,...)return A.new(...)end})function a.new(B,C,D,E)assert(#B==#C,"Expected same number of data for both axis")local self=setmetatable({},a)self.name=D;self:setData(B,C)self.col=E or math.random(0x000000,0xFFFFFF)return self end;function a:getName()return self.name end;function a:getDX()return self.dx end;function a:getDY()return self.dy end;function a:getColor()return self.col end;function a:getMinX()return self.minX end;function a:getMinY()return self.minY end;function a:getMaxX()return self.maxX end;function a:getMaxY()return self.maxY end;function a:getDataLength()return#self.dx end;function a:getLineWidth()return self.lWidth or 3 end;function a:setName(D)self.name=D end;function a:setData(B,C)self.dx=B;self.dy=C;self.minX=c(B)self.minY=c(C)self.maxX=d(B)self.maxY=d(C)end;function a:setColor(E)self.col=E end;function a:setLineWidth(F)self.lWidth=F end;b={}b.__index=b;b._joints=10000;setmetatable(b,{__call=function(A,...)return A.new(...)end})function b.init()tfm.exec.addPhysicObject(-1,0,0,{type=14,miceCollision=false,groundCollision=false})end;function b.handleClick(G,H,I)if I:sub(0,("lchart:data:["):len())=='lchart:data:['then local J=split(I:sub(("lchart:data:["):len()+1,-2),",")local K,L,M,N=split(J[1],":")[2],split(J[2],":")[2],split(J[3],":")[2],split(J[4],":")[2]ui.addTextArea(18000,"<a href='event:chart_close'>X: "..M.."<br>Y: "..N.."</a>",H,K,L,80,30,nil,nil,0.5,false)elseif I=="chart_close"then ui.removeTextArea(G,H)end end;function b.new(G,O,z,F,P)local self=setmetatable({},b)self.id=G;self.x=O;self.y=z;self.w=F;self.h=P;self.showing=false;self.joints=b._joints;b._joints=b._joints+10000;self.series={}return self end;function b:getId()return self.id end;function b:getDimension()return{x=self.x,y=self.y,w=self.w,h=self.h}end;function b:getMinX()return self.minX end;function b:getMaxX()return self.maxX end;function b:getMinY()return self.minY end;function b:getMaxY()return self.maxY end;function b:getXRange()return self.xRange end;function b:getYRange()return self.yRange end;function b:getGraphColor()return{bgColor=self.bg or 0x324650,borderColor=self.border or 0x212F36}end;function b:getAlpha()return self.alpha or 0.5 end;function b:isShowing()return self.showing end;function b:getDataLength()local Q=0;for R,i in next,self.series do Q=Q+i:getDataLength()end;return Q end;function b:show()self:refresh()local S,T=math.floor,math.ceil;ui.addTextArea(10000+self.id,"",nil,self.x,self.y,self.w,self.h,self.bg,self.border,self:getAlpha(),false)ui.addTextArea(11000+self.id,"<b>["..S(self.minX)..", "..S(self.minY).."]</b>",nil,self.x-15,self.y+self.h+5,50,50,nil,nil,0,false)ui.addTextArea(12000+self.id,"<b>"..T(self.maxX).."</b>",nil,self.x+self.w+10,self.y+self.h+5,50,50,nil,nil,0,false)ui.addTextArea(13000+self.id,"<b>"..T(self.maxY).."</b>",nil,self.x-15,self.y-10,50,50,nil,nil,0,false)ui.addTextArea(14000+self.id,"<b>"..T((self.maxX+self.minX)/2).."</b>",nil,self.x+self.w/2,self.y+self.h+5,50,50,nil,nil,0,false)ui.addTextArea(15000+self.id,"<br><br><b>"..T((self.maxY+self.minY)/2).."</b>",nil,self.x-15,self.y+(self.h-self.y)/2,50,50,nil,nil,0,false)local U=self.joints;local V=self.w/self.xRange;local W=self.h/self.yRange;for G,X in next,self.series do for Y=1,X:getDataLength(),1 do local Z=S(X:getDX()[Y]*V+self.x-self.minX*V)local _=S(y(X:getDY()[Y]*W)+self.y-y(self.h)+self.minY*W)local a0=S((X:getDX()[Y+1]or X:getDX()[Y])*V+self.x-self.minX*V)local a1=S(y((X:getDY()[Y+1]or X:getDY()[Y])*W)+self.y-y(self.h)+self.minY*W)tfm.exec.addJoint(self.id+6+U,-1,-1,{type=0,point1=Z..",".._,point2=a0 ..","..a1,damping=0.2,line=X:getLineWidth(),color=X:getColor(),alpha=1,foreground=true})if self.showDPoints then ui.addTextArea(16000+self.id+U,"<font color='#"..num2hex(X:getColor()).."'><a href='event:lchart:data:[x:"..Z..",y:".._..",dx:"..X:getDX()[Y]..",dy:"..X:getDY()[Y].."]'>█</a></font>",nil,Z,_,10,10,nil,nil,0,false)end;U=U+1 end end;self.showing=true end;function b:setGraphColor(a2,a3)self.bg=a2;self.border=a3 end;function b:setShowDataPoints(a4)self.showDPoints=a4 end;function b:setAlpha(a5)self.alpha=a5 end;function b:addSeries(X)table.insert(self.series,X)self:refresh()end;function b:removeSeries(D)for x=1,#self.series do if self.series[x]:getName()==D then table.remove(self.series,x)break end end;self:refresh()end;function b:refresh()self.minX,self.minY,self.maxX,self.maxY=nil;for s,i in next,self.series do self.minX=math.min(i:getMinX(),self.minX or i:getMinX())self.minY=math.min(i:getMinY(),self.minY or i:getMinY())self.maxX=math.max(i:getMaxX(),self.maxX or i:getMaxX())self.maxY=math.max(i:getMaxY(),self.maxY or i:getMaxY())end;self.xRange=self.maxX-self.minX;self.yRange=self.maxY-self.minY end;function b:resize(F,P)self.w=F;self.h=P end;function b:move(O,z)self.x=O;self.y=z end;function b:hide()for G=10000,17000,1000 do ui.removeTextArea(G+self.id)end;for G=self.id+16000,self.joints,1 do ui.removeTextArea(G+self.id)end;for Y=self.joints,self.joints+self:getDataLength()+5,1 do tfm.exec.removeJoint(Y)end;self.showing=false end;function b:showLabels(a4)if a4 or a4==nil then local a6=""for R,X in next,self.series do a6=a6 .."<font color='#"..num2hex(X:getColor()).."'> ▉<b> "..X:getName().."</b></font><br>"end;ui.addTextArea(16000+self.id,a6,nil,self.x+self.w+15,self.y,100,18*#self.series,self:getGraphColor().bgColor,self:getGraphColor().borderColor,self:getAlpha(),false)else ui.removeTextArea(16000+self.id,nil)end end;function b:displayGrids(a4)if a4 or a4==nil then local a7=self.h/5;for G,z in next,f(self.y+a7,self.y+self.h-a7,a7)do tfm.exec.addJoint(self.id+G,-1,-1,{type=0,point1=self.x..","..z,point2=self.x+self.w..","..z,damping=0.2,line=1,alpha=1,foreground=true,color=0xFFFFFF})end;tfm.exec.addJoint(self.id+5,-1,-1,{type=0,point1=self.x+self.w/2 ..","..self.y,point2=self.x+self.w/2 ..","..self.y+self.h,damping=0.2,line=2,alpha=1,foreground=true,color=0xFFFFFF})tfm.exec.addJoint(self.id+6,-1,-1,{type=0,point1=self.x..","..self.y+self.h/2,point2=self.x+self.w..","..self.y+self.h/2,damping=0.2,line=2,alpha=1,foreground=true,color=0xFFFFFF})end end;Series=a;LineChart=b;getMin=c;getMax=d;map=e;range=f
+    --[ timers4tfm ]--
+    local a={}a.__index=a;a._timers={}setmetatable(a,{__call=function(b,...)return b.new(...)end})function a.process()local c=os.time()local d={}for e,f in next,a._timers do if f.isAlive and f.mature<=c then f:call()if f.loop then f:reset()else f:kill()d[#d+1]=e end end end;for e,f in next,d do a._timers[f]=nil end end;function a.new(g,h,i,j,...)local self=setmetatable({},a)self.id=g;self.callback=h;self.timeout=i;self.isAlive=true;self.mature=os.time()+i;self.loop=j;self.args={...}a._timers[g]=self;return self end;function a:setCallback(k)self.callback=k end;function a:addTime(c)self.mature=self.mature+c end;function a:setLoop(j)self.loop=j end;function a:setArgs(...)self.args={...}end;function a:call()self.callback(table.unpack(self.args))end;function a:kill()self.isAlive=false end;function a:reset()self.mature=os.time()+self.timeout end;Timer=a
 
+    local gameMap = [[<C><P Ca=""/><Z><S><S X="401" Y="400" T="16" L="807" H="28" P="0,0,0.3,0.2,0,0,0,0"/><S X="398" Y="-17" T="16" L="807" H="28" P="0,0,0.3,0.2,0,0,0,0"/><S X="-19" Y="198" T="16" L="35" H="458" P="0,0,0.3,0.2,0,0,0,0"/><S X="817" Y="197" T="16" L="28" H="453" P="0,0,0.3,0.2,0,0,0,0"/></S><D/><O/><L/></Z></C>]]
+
+    system.disableChatCommandDisplay()
+    tfm.exec.disableAfkDeath()
+    tfm.exec.disableAutoNewGame()
+    tfm.exec.disableAutoShaman()
+    tfm.exec.newGame(gameMap)
+
+    LineChart.init()
+
+        
+    local chart = LineChart(1, 180, 50, 480, 250)
+    local series = Series(
+        {
+            0, 0, 20, 20, 40, 40, 60, 60, 80, 80, 80, 100, 100, 100, 400, 400, 420, 430, 430, 450, 450, 470, 470, 490, 490, 510, 510
+        }, {
+            0, 50, 50, 40, 40, 50, 50, 40, 40, 50, 50, 50, 50, 30, 30, 50, 50, 50, 40, 40, 50, 50, 40, 40, 50, 50, 0
+        },
+    "Castle", 0xFFCC00)
+    local timer = Timer(1, function() end, 500, true)
+    chart:addSeries(series)
+    chart:showLabels(true)
+    
+    local graphs = {
+        ["y = sin x"] = function()
+            local currX = 0
+            timer:setCallback(function()
+                local x = range(currX, currX + 10, 0.3)
+	            local y = map(x, function(x) return  math.sin(x) end )
+                series:setData(x, y)
+                series:setName("y = sin x")
+                series:setColor(0xFF0000)
+                chart:show()
+                chart:showLabels(true)
+	            currX = currX + 0.5
+            end)
+            timer:setLoop(true)
+            timer:call()
+        end,
+        ["y = cos x"] = function()
+            local currX = 0
+            timer:setCallback(function()
+                local x = range(currX, currX + 10, 0.3)
+	            local y = map(x, function(x) return  math.cos(x) end )
+                series:setData(x, y)
+                series:setColor(0x00FF00)
+                chart:show()
+                chart:showLabels(true)
+	            currX = currX + 0.5
+            end)
+            timer:setLoop(true)
+            timer:call()
+        end,
+        ["y = tan x"] = function()
+            local currX = 0
+            timer:setCallback(function()
+                local x = range(currX, currX + 10, 0.3)
+	            local y = map(x, function(x) return math.tan(x) end )
+                series:setData(x, y)
+                series:setColor(0x0000FF)
+                chart:show()
+                chart:showLabels(true)
+	            currX = currX + 0.5
+            end)
+            timer:setLoop(true)
+            timer:call()
+        end,
+        ["Untitled"] = function()
+            local currX = 0
+            timer:setCallback(function()
+                local x = range(currX, currX + 10, 0.3)
+                local y = map(x, function(x) return math.sin(2 * math.sin(2 * math.sin(2 * math.sin(x)))) end )
+                series:setData(x, y)
+                series:setColor(0x00FFFF)
+                chart:show()
+                chart:showLabels(true)
+                currX = currX + 0.5
+            end)
+            timer:setLoop(true)
+            timer:call()
+        end,
+        ["Castle"] = function()
+            chart:hide()
+            timer:setCallback(function()
+                series:setData({
+                    0, 0, 20, 20, 40, 40, 60, 60, 80, 80, 80, 100, 100, 100, 400, 400, 420, 430, 430, 450, 450, 470, 470, 490, 490, 510, 510
+                }, {
+                    0, 50, 50, 40, 40, 50, 50, 40, 40, 50, 50, 50, 50, 30, 30, 50, 50, 50, 40, 40, 50, 50, 40, 40, 50, 50, 0
+                })
+                series:setColor(0xFFCC00)
+                chart:show()
+            end)
+            timer:setLoop(true)
+            timer:call()
+            chart:showLabels(true)
+        end
+    }
+
+    -- [[ events ]] --
+    eventNewPlayer = function(name)
+        local commu = tfm.get.room.playerList[name].community
+        tfm.exec.chatMessage(module.translate("welcome0graphs", commu), name)
+        chart:show()
+        local isRoomAdmin = module.subRoomAdmins[name]
+        local counter = 0
+        for graph in next, graphs do
+            ui.addTextArea(counter, "<b><p align='center'>" .. (isRoomAdmin and "<a href='event:graph:" .. graph .. "'>" or "<N2>") .. graph .. (isRoomAdmin and "</a>" or "</N2>") .. "</p></b>", name, 50, 50 + counter * 30, 100, 20, nil, nil, 1, true)
+            counter = counter + 1
+        end
+    end
+    
+    eventNewGame = function()
+        for name, player in next, tfm.get.room.playerList do eventNewPlayer(name) end
+    end
+
+    eventLoop = function(tc, tr)
+        Timer.process()
+    end
+
+    eventTextAreaCallback = function(id, name, evt)
+        if evt:find("%w+:.+") then
+            local key, value = table.unpack(string.split(evt, ":"))
+            if key == "graph" and module.subRoomAdmins[name] then
+                graphs[value]()
+                series:setName(value)
+            end
+        end
+    end
+
+    eventChatCommand = function(name, cmd)
+        local commu = tfm.get.room.playerList[name].community
+        local args = string.split(cmd, " ")
+        print(table.tostring(args))
+        if args[1] == "commands" then
+            tfm.exec.chatMessage(module.translate("cmds0graphs"), name)
+        elseif args[1] == "admin" and tfm.get.room.playerList[args[2]] then
+            if module.subRoomAdmins[name] then
+                module.subRoomAdmins[args[2]] = true
+                tfm.exec.chatMessage(module.translate("new_roomadmin", tfm.get.room.community, nil, {name = args[2]}))
+                local counter = 0
+                for graph in next, graphs do
+                    ui.addTextArea(counter, "<b><p align='center'><a href='event:graph:" .. graph .. "'>" .. graph .. "</a></p></b>", args[2], 50, 50 + counter * 30, 100, 20, nil, nil, 1, true)
+                    counter = counter + 1
+                end
+            else
+                tfm.exec.chatMessage(module.translate("error_auth", commu), name)
+            end
+        end
+    end
+
+end
 
 
 if modes[module.mode] then
